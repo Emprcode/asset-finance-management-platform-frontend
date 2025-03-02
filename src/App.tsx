f7c3d243b3ad31a1def4aa326168d01472d5c180
Narayan Aryal <aryal.coding@gmail.com>
2025-03-02 11:29:55 +1100

    added reuseable form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
-import AddApplication from "./pages/AddApplication";
+import AddApplications from "./pages/AddApplications";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard />} />
-        <Route path='/add-application' element={<AddApplication />} />
+        <Route path='/add-application' element={<AddApplications />} />
       </Routes>
     </BrowserRouter>
   );
